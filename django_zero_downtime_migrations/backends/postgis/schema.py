188134aff3f0f3bd3c3869587edef2479b7badd4
Paveł Tyślacki <pavel.tysliatski@datarobot.com>
2019-09-25 01:47:12 +0300

    fix isort

--- django_zero_downtime_migrations/backends/postgis/schema.py
+++ django_zero_downtime_migrations/backends/postgis/schema.py
@@ -1,6 +1,8 @@
 from django.contrib.gis.db.backends.postgis.schema import PostGISSchemaEditor
 
-from django_zero_downtime_migrations.backends.postgres.schema import DatabaseSchemaEditorMixin
+from django_zero_downtime_migrations.backends.postgres.schema import (
+    DatabaseSchemaEditorMixin
+)
 
 
 class DatabaseSchemaEditor(DatabaseSchemaEditorMixin, PostGISSchemaEditor):
