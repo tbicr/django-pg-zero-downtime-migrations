8766f85aa228be6dcc68f5157ba4256129d1e559
Bruno Alla <alla.brunoo@gmail.com>
2024-02-06 13:07:58 +0000

    Add Django 5.0 to the CI & supported versions

--- setup.py
+++ setup.py
@@ -46,6 +46,7 @@ setup(
         'Framework :: Django :: 4.0',
         'Framework :: Django :: 4.1',
         'Framework :: Django :: 4.2',
+        'Framework :: Django :: 5.0',
     ],
     keywords='django postgres postgresql migrations',
     packages=find_packages(exclude=['manage*', 'tests*']),
