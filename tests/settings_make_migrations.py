19c7c07daa5dd2ded5a6c28ca010495da40c2dd8
Paveł Tyślacki <pavel.tyslacki@gmail.com>
2026-08-26 00:54:53 +0100

    drop stale traces of the unsupported postgres versions

--- tests/settings_make_migrations.py
+++ tests/settings_make_migrations.py
@@ -11,7 +11,6 @@ INSTALLED_APPS += [  # noqa: F405
     'tests.apps.bad_flow_add_column_with_notnull_default_app',
     'tests.apps.bad_flow_add_column_with_notnull_app',
     'tests.apps.bad_flow_change_char_type_that_unsafe_app',
-    'tests.apps.old_notnull_check_constraint_migration_app',
     'tests.apps.good_flow_drop_table_with_constraints',
     'tests.apps.good_flow_drop_column_with_constraints',
     'tests.apps.idempotency_create_table_app',
